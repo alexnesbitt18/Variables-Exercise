feedc9d8d4f1a35e17bc9dded121e8039ef4d508
alexnesbitt18 <alexnesbitt18@gmail.com>
2023-08-25 10:15:26 -0700

    Wrote sentences using multiple value and reference types

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string personName = "Alex";
+            int personAge = 30; 
+            char middleInitial = 'R';
+            bool isMarried = true;
+            double currentTemp = 60.5;
+            decimal currentPrice = 4.99m;
+
+            Console.WriteLine($"Hello, World! My name is {personName} , and I am {personAge} years old. ");
+            Console.WriteLine($"My middle initial is {middleInitial} and if you were to ask me if I were married, I would have to say that it is {isMarried}.");
+            Console.WriteLine($"The current temperature outside currently is {currentTemp} degrees and the price of my favorite hamburger is {currentPrice} US dollars.");
+
+
+
         }
     }
 }
